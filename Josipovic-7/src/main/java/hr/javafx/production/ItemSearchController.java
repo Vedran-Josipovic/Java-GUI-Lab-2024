8b0b78dca12a852a81c5c48a0cb38343bca59d6a
Vedran-Josipovic <vedranjosipovic.posao@gmail.com>
2023-12-02 20:27:45 +0100

    Initialized items in ItemSearchController

--- Josipovic-7/src/main/java/hr/javafx/production/ItemSearchController.java
+++ Josipovic-7/src/main/java/hr/javafx/production/ItemSearchController.java
@@ -3,14 +3,16 @@ package hr.javafx.production;
 import hr.java.production.model.Category;
 import hr.java.production.model.Item;
 import hr.java.production.utility.FileUtils;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.util.Callback;
 
+import java.nio.file.Path;
+import java.time.LocalDate;
 import java.util.List;
 
 public class ItemSearchController {
@@ -37,8 +39,16 @@ public class ItemSearchController {
     private TableColumn<Item, String> itemSellingPriceTableColumn;
 
     public void initialize(){
-
+        itemNameTableColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue().getName()));
+        itemCategoryTableColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue().getCategory().getName()));
+        itemWidthTableColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue().getWidth().toString()));
+        itemHeightTableColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue().getHeight().toString()));
+        itemLengthTableColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue().getLength().toString()));
+        itemProductionCostTableColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue().getProductionCost().toString()));
+        itemSellingPriceTableColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue().getSellingPrice().toString()));
     }
+
+
 
     public void itemSearch() {
         List<Category> categories = FileUtils.inputCategories();
